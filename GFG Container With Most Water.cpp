102fe5078dce3b3b18eb504d6cd3809c0daf9dc2
Vikrant Kumar <159226321+Vikrant076@users.noreply.github.com>
2025-08-31 01:57:12 +0530

    Create GFG Container With Most Water.cpp

---
+++ GFG Container With Most Water.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    int maxWater(vector<int> &arr) {
+        int left = 0, right = arr.size() - 1;
+        int maxWater = 0;
+        
+        while (left < right) {
+            int height = min(arr[left], arr[right]);
+            int width = right - left;
+            maxWater = max(maxWater, height * width);
+            
+            if (arr[left] < arr[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        
+        return maxWater;
+    }
+};
